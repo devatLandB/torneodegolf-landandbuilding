6be1dc3942faa2068d8c441612313a797a7134f5
devatLandB <dev@landandbuilding.com>
2024-03-21 16:39:39 -0700

    3rd commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+],
+	theme: {
+		extend: {
+			colors: {
+				'primary': '#002554',
+			},
+			fontFamily: {
+				'sans': ['Montserrat'],
+			},
+		},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+};
